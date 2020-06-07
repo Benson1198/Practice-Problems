75b66ba481f32fe6e962cfc7fd7624bd63d8761d
Benson P Xavier <34964177+Benson1198@users.noreply.github.com>
2020-06-07 13:41:04 +0530

    first commit

---
+++ Linked Lists/Detect loop in LL(GFG).py
@@ -0,0 +1,12 @@
+def detectLoop(head):
+    marker1 = head
+    marker2 = head
+    
+    while marker1 != None and marker2.next != None:
+        marker1 = head.next
+        marker2 = head.next.next
+        
+    
+        if marker1 == marker2:
+            return True
+    return False
