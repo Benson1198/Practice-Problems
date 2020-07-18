84ee197379f49f57d352f9b649fb6a4838edd918
Benson P Xavier <34964177+Benson1198@users.noreply.github.com>
2020-07-18 13:50:07 +0530

    Stacks Prob.

--- Stacks/Next Larger Element.py
+++ Stacks/Next Larger Element.py
@@ -67,3 +67,39 @@ def nextLargerElement(arr,n):
         push(s, [next,i])
     
     return next_greater
+
+
+
+# My Solution using STACKS
+
+def nextLargerElement(arr):
+    n = len(arr)
+
+    stack = []
+
+    v = []
+
+    for i in range(n-1,-1,-1):
+        if len(stack) == 0:
+            v.append(-1)
+        elif len(stack) > 0 and arr[i] < stack[-1]:
+            v.append(stack[-1])
+        elif len(stack) > 0 and stack[-1] <= arr[i]:
+
+            while len(stack) > 0 and stack[-1] <= arr[i]:
+                stack.pop()
+            
+            if len(stack) == 0:
+                v.append(-1)
+            else:
+                v.append(stack[-1])
+        
+        stack.append(arr[i])
+
+    v.reverse()
+    
+    return v
+
+arr = [1,3,2,4]
+
+print(nextLargerElement(arr))
